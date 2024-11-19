e13040b747179e56e96b10248f4d08ba471b26f6
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-11-19 12:44:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-var nodeFeatures = [];
 var vertices = d3.map();
 var activeMapType = 'nodes_links';
 var map = d3.select('#map');
 var mapWidth = +map.attr('width');
 var mapHeight = +map.attr('height');
 
+// Leaflet map setup
 var atlLatLng = new L.LatLng(33.7771, -84.3900);
 var myMap = L.map('map').setView(atlLatLng, 5);
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	     {
 	       maxZoom: 10,
@@ -16,36 +15,46 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
              }).addTo(myMap);
 
              
+             
 var svgLayer = L.svg();
 svgLayer.addTo(myMap)
              
 var svg = d3.select('#map').select('svg');
 var nodeLinkG = svg.select('g')
 .attr('class', 'leaflet-zoom-hide');
-             
-
-
 
 Promise.all([
     d3.csv('gridkit_north_america-highvoltage-vertices.csv', function(row) {
-        return {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
-                    voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
-    }),
-    d3.csv('gridkit_north_america-highvoltage-links.csv', function(row) {
-        return {l_id: +row['l_id'], v_id_1: +row['v_id_1'], v_id_2: +row['v_id_2'],
-                  voltage: +row['voltage'], cables: +row['cables'], wires: +row['wires'],
-                  frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+        var node = {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
+          voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+          vertices.set(node.v_id, node);
+          node.linkCount = 0;
+          return node;
 
     }),
-    d3.json('states.json')     
+    d3.csv('gridkit_north_america-highvoltage-links.csv', function(row) {
+        var link = {l_id: +row['l_id'], v_id_1: +row['v_id_1'], v_id_2: +row['v_id_2'],
+                   voltage: +row['voltage'], cables: +row['cables'], wires: +row['wires'],
+                   frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+               link.node1 = vertices.get(link.v_id_1);
+               link.node2 = vertices.get(link.v_id_2);
+               link.node1.linkCount += 1;
+               link.node2.linkCount += 1;
+               return link
+        
+    })
+        
 ]).then(function(data) {
     var nodes = data[0];
     var links = data[1];
-    readyToDraw(nodes, links, states)
+    readyToDraw(nodes, links)
 });
 
+var linkCountExtent = d3.extent(nodes, function(d) {return d.linkCount;});
+var radiusScale = d3.scaleSqrt().range([0.5,7.5]).domain(linkCountExtent);
 
-function readyToDraw(nodes, links, states) {
+
+function readyToDraw(nodes, links) {
     nodeLinkG.selectAll('.grid-link')
     .data(links)
     .enter().append('line')
@@ -65,54 +74,8 @@ function readyToDraw(nodes, links, states) {
         });   
         myMap.on('zoomend', updateLayers);
         updateLayers();
-        
-        var nodeCollection = turf.featureCollection(nodeFeatures);
-        var chorostates = turf.collect(states, nodeCollection, 'v_id', 'values')
-        var bbox = turf.bbox(nodeCollection);
-        var cellSize = 250;
-        var options = {units: 'kilometers'};
-        var triangleGrid = turf.triangleGrid(bbox, cellSize, options);
-        var triangleBins = turf.collect(triangleGrid, nodeCollection, 'v_id', 'values');
-        triangleBins.features = triangleBins.features.filter(function(d){
-        return d.properties.values.length > 0;
-    });
-
-        var triangleLayer = L.geoJson(triangleBins, {style: triangleStyle});
-        triangleLayer.addTo(myMap);
-
-        var triangleExtent = d3.extent(triangleBins.features, function(d){
-            return d.properties.values.length;
-        });
-        var triangleScale = d3.scaleSequential(d3.interpolateMagma)
-            .domain(triangleExtent.reverse());
-        
-        var triangleStyle = function(f) {
-                return {
-                    weight: 0.5,
-                    opacity: 1,
-                    color: 'white',
-                    fillOpacity: 0.7,
-                    fillColor: triangleScale(f.properties.values.length)
-                }
-            };
-        
-
-        var statesStyle = function(f) {
-            return {
-                weight: 2,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: 0.7,
-                fillColor: choroScale(f.properties.values.length)
-            }
-        };
-
-        
-        statesLayer = L.geoJson(chorostates, {style: statesStyle});
-        var choroScale = d3.scaleThreshold()
-	.domain([10,20,50,100,200,500,1000])
-	.range(d3.schemeYlOrRd[8]);
+        var nodeTypes = d3.map(nodes, function(d){return d.type;}).keys();
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(nodeTypes);
 
 }
 
@@ -125,6 +88,7 @@ function updateLayers(){
        .attr('y1', function(d){return myMap.latLngToLayerPoint(d.node1.LatLng).y})
        .attr('x2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).x})
        .attr('y2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).y});
+
 };
 
 d3.selectAll('.btn-group > .btn.btn-secondary')
@@ -145,13 +109,6 @@ d3.selectAll('.btn-group > .btn.btn-secondary')
             case 'nodes_links':
                 nodeLinkG.attr('visibility', 'hidden');
                 break;
-            case 'states':
-                myMap.removeLayer(statesLayer);
-                break;
-            case 'triangle_bins':
-               myMap.removeLayer(triangleLayer);
-               break;
-
         }
     }
     
@@ -162,13 +119,7 @@ d3.selectAll('.btn-group > .btn.btn-secondary')
             case 'nodes_links':
                 nodeLinkG.attr('visibility', 'visible');
                 break;
-            case 'states':
-                statesLayer.addTo(myMap);
-                break;
-            case 'triangle_bins':
-                    triangleLayer.addTo(myMap);
-                    break;
-                    
-                    
         }
     }
+    
+    
