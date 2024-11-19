e13e697d9edf9b64205006e0fcdd220a4853f2f7
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-11-19 13:12:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var vertices = d3.map();
 var map = d3.select('#map');
 var mapWidth = +map.attr('width');
 var mapHeight = +map.attr('height');
 var atlLatLng = new L.LatLng(33.7771, -84.3900);
 var myMap = L.map('map').setView(atlLatLng, 5);
+var activeMapType = 'nodes_links';
 
 var svgLayer = L.svg();
 svgLayer.addTo(myMap)
@@ -13,15 +15,26 @@ var nodeLinkG = svg.select('g')
 
 Promise.all([
         d3.csv('gridkit_north_america-highvoltage-vertices.csv', function(row) {
-            return {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
-                voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
-        })
+            var node = {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
+           voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+       vertices.set(node.v_id, node);
+       return node;
+        }),
+        d3.csv('gridkit_north_america-highvoltage-links.csv', function(row) {
+            var link = {l_id: +row['l_id'], v_id_1: +row['v_id_1'], v_id_2: +row['v_id_2'],
+            voltage: +row['voltage'], cables: +row['cables'], wires: +row['wires'],
+            frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+            link.node1 = vertices.get(link.v_id_1);
+            link.node2 = vertices.get(link.v_id_2);
+       return link;
+    })     
     ]).then(function(data) {
         var nodes = data[0];
-        readyToDraw(nodes)
+        var links = data[1];
+        readyToDraw(nodes, links)
     });
 
-function readyToDraw(nodes) {
+function readyToDraw(nodes, links) {
         nodeLinkG.selectAll('.grid-node')
             .data(nodes)
             .enter().append('circle')
@@ -30,8 +43,15 @@ function readyToDraw(nodes) {
             .style('fill-opacity', 0.6)
             .attr('r', 2);
 
-            myMap.on('zoomend', updateLayers);
-            updateLayers();
+        nodeLinkG.selectAll('.grid-link')
+            .data(links)
+            .enter().append('line')
+            .attr('class', 'grid-link')
+            .style('stroke', '#999')
+            .style('stroke-opacity', 0.5);
+        
+        myMap.on('zoomend', updateLayers);
+        updateLayers();
 
 }
 
@@ -39,6 +59,13 @@ function updateLayers(){
     nodeLinkG.selectAll('.grid-node')
     .attr('cx', function(d){return myMap.latLngToLayerPoint(d.LatLng).x})
     .attr('cy', function(d){return myMap.latLngToLayerPoint(d.LatLng).y})
+
+    nodeLinkG.selectAll('.grid-link')
+       .attr('x1', function(d){return myMap.latLngToLayerPoint(d.node1.LatLng).x})
+       .attr('y1', function(d){return myMap.latLngToLayerPoint(d.node1.LatLng).y})
+       .attr('x2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).x})
+       .attr('y2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).y});
+
 };
     
 
@@ -50,6 +77,41 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
              }).addTo(myMap);
 
 
+d3.selectAll('.btn-group > .btn.btn-secondary')
+             .on('click', function() {
+                 var newMapType = d3.select(this).attr('data-type');
+         
+                 d3.selectAll('.btn.btn-secondary.active').classed('active', false);
+         
+                 cleanUpMap(activeMapType);
+                 showOnMap(newMapType);
+         
+                 activeMapType = newMapType;
+             });
+         
+             function cleanUpMap(type) {
+                switch(type) {
+                    case 'cleared':
+                        break;
+                    case 'nodes_links':
+                        nodeLinkG.attr('visibility', 'hidden');
+                        break;
+                }
+            }
+            
+            function showOnMap(type) {
+                switch(type) {
+                    case 'cleared':
+                        break;
+                    case 'nodes_links':
+                        nodeLinkG.attr('visibility', 'visible');
+                        break;
+                }
+            }
+            
+
+
+             
 
 
 
