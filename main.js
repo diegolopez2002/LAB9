ca6e49898fe38d510a9d8b9bf618f759ae187065
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-11-19 01:51:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var map = d3.select('#map');
+var vertices = d3.map();
 var mapWidth = +map.attr('width');
 var mapHeight = +map.attr('height');
-
+var activeMapType = 'nodes_links';
 var atlLatLng = new L.LatLng(33.7771, -84.3900);
 var myMap = L.map('map').setView(atlLatLng, 5);
 
@@ -22,15 +23,28 @@ var nodeLinkG = svg.select('g')
 
 Promise.all([
         d3.csv('gridkit_north_america-highvoltage-vertices.csv', function(row) {
-            return {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
-                voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
-        })
+            var node = {v_id: +row['v_id'], LatLng: [+row['lat'], +row['lng']], type: row['type'],
+            voltage: +row['voltage'], frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+            vertices.set(node.v_id, node);
+            return node;
+        }),
+        d3.csv('gridkit_north_america-highvoltage-links.csv', function(row) {
+            var link = {l_id: +row['l_id'], v_id_1: +row['v_id_1'], v_id_2: +row['v_id_2'],
+                       voltage: +row['voltage'], cables: +row['cables'], wires: +row['wires'],
+                       frequency: +row['frequency'], wkt_srid_4326: row['wkt_srid_4326']};
+                   link.node1 = vertices.get(link.v_id_1);
+                   link.node2 = vertices.get(link.v_id_2);
+                   return link;
+            
+                })
+            
     ]).then(function(data) {
         var nodes = data[0];
-        readyToDraw(nodes)
+        var links = data[1];
+        readyToDraw(nodes, links);
     });
     
-function readyToDraw(nodes) {
+function readyToDraw(nodes, links) {
         nodeLinkG.selectAll('.grid-node')
             .data(nodes)
             .enter().append('circle')
@@ -38,15 +52,61 @@ function readyToDraw(nodes) {
             .style('fill', 'red')
             .style('fill-opacity', 0.6)
             .attr('r', 2);
+        nodeLinkG.selectAll('.grid-link')
+            .data(links)
+            .enter().append('line')
+            .attr('class', 'grid-link')
+            .style('stroke', '#999')
+            .style('stroke-opacity', 0.5);
+        
 
-            myMap.on('zoomend', updateLayers);
-            updateLayers();
+        myMap.on('zoomend', updateLayers);
+        updateLayers();
 
 }
+
+
 
 function updateLayers(){
     nodeLinkG.selectAll('.grid-node')
     .attr('cx', function(d){return myMap.latLngToLayerPoint(d.LatLng).x})
-    .attr('cy', function(d){return myMap.latLngToLayerPoint(d.LatLng).y})
+    .attr('cy', function(d){return myMap.latLngToLayerPoint(d.LatLng).y});
+    nodeLinkG.selectAll('.grid-link')
+           .attr('x1', function(d){return myMap.latLngToLayerPoint(d.node1.LatLng).x})
+           .attr('y1', function(d){return myMap.latLngToLayerPoint(d.node1.LatLng).y})
+           .attr('x2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).x})
+           .attr('y2', function(d){return myMap.latLngToLayerPoint(d.node2.LatLng).y});
+    
 };
+d3.selectAll('.btn-group > .btn.btn-secondary')
+    .on('click', function() {
+        var newMapType = d3.select(this).attr('data-type');
+
+        d3.selectAll('.btn.btn-secondary.active').classed('active', false);
+
+        cleanUpMap(activeMapType);
+        showOnMap(newMapType);
+
+        activeMapType = newMapType;
+    });
+
+    function cleanUpMap(type) {
+        switch(type) {
+            case 'cleared':
+                break;
+            case 'nodes_links':
+                nodeLinkG.attr('visibility', 'hidden');
+                break;
+        }
+    }
+    
+    function showOnMap(type) {
+        switch(type) {
+            case 'cleared':
+                break;
+            case 'nodes_links':
+                nodeLinkG.attr('visibility', 'visible');
+                break;
+        }
+    }
     
